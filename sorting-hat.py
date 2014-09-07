ebe7b9a692f582f657c9c4d93dd90c887bc86e93
Rachel <rachelb@gmail.com>
2014-09-07 16:35:14 -0700

    Create sorting-hat.py

---
+++ sorting-hat.py
@@ -0,0 +1,19 @@
+name = (raw_input("Please enter your first name.  "))
+birthday = (raw_input("Please enter your birth month (ex. January).  "))
+birth_place = (raw_input("Please enter the city where you were born.  "))
+lhp = int(raw_input("Please enter how much you enjoyed the Harry Potter series \non a scale between 1 and 10 (10 being LOVED it!).  "))
+
+print "You will now be sorted into your Hogwarts House."
+
+import random
+
+house = random.randrange(1, 4)
+
+if house == 1:
+	print "You belong to the house of of Helga Hufflepuff."
+elif house == 2:
+	print "You belong to the house of Rowena Ravenclaw."
+elif house == 3:
+	print "You belong to the house of Godric Gryffindor."
+elif house == 4:
+	print "You belong to the hous eof Salazar Slytherin."
