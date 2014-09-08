dc0ce803b7ff57e191a1d1efc46c89482de7c04f
Rachel <rachelb@gmail.com>
2014-09-07 20:36:13 -0700

    Update sorting-hat.py

--- sorting-hat.py
+++ sorting-hat.py
@@ -7,7 +7,7 @@ print "You will now be sorted into your Hogwarts House."
 
 import random
 
-house = random.randrange(1, 4)
+house = random.randrange(0, 4)
 
 if house == 1:
 	print "You belong to the house of of Helga Hufflepuff."
